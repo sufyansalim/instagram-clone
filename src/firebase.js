97de7a3336f23ef387bbc0b95177a9cd791c9978
Sufyan Saleem <40366969+sufyansalim@users.noreply.github.com>
2021-04-01 02:56:59 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase"
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+
+export  {db,auth,storage};
